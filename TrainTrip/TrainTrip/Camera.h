dd4455751176fb43ad1799e09f791de0dca3d75a
Tibicuy <tiby.onofrei@student.unitbv.ro>
2023-11-29 10:48:51 +0200

    I added Camera class

---
+++ TrainTrip/TrainTrip/Camera.h
@@ -0,0 +1,23 @@
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h> 
+
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+
+class Camera
+{
+};
+#endif
