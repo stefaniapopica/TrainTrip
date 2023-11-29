a14dbecd1c693798cf77ad0eda580562b5e929ce
alexandra-marin29 <alexandra-a.marin@student.unitbv.ro>
2023-11-29 12:01:35 +0200

    mesh constructor added

--- TrainTrip/TrainTrip/Mesh.cpp
+++ TrainTrip/TrainTrip/Mesh.cpp
@@ -1,1 +1,11 @@
 #include "Mesh.h"
+
+
+Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+{
+    this->vertices = vertices;
+    this->indices = indices;
+    this->textures = textures;
+
+    setupMesh();
+}
