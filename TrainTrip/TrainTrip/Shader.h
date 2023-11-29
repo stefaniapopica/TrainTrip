f90d64e47e1b7b0d26d49cc78176099dfc30dd47
lucianamihai <luciana.mihai@student.unitbv.ro>
2023-11-29 12:52:19 +0200

    Modified directories path & shader class created

---
+++ TrainTrip/TrainTrip/Shader.h
@@ -0,0 +1,33 @@
+
+#ifndef SHADER_H
+#define SHADER_H
+
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+#include<cerrno>
+
+#include <stdlib.h> // necesare pentru citirea shader-elor
+#include <stdio.h>
+#include <math.h> 
+
+//#include <GL/glew.h>
+
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <glad/glad.h>
+#include <glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+class Shader
+{
+};
+
+#endif
