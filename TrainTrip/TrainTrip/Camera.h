4ca63bd51dedcb5040e5abe7ea9d15b25ad46303
Tibicuy <tiby.onofrei@student.unitbv.ro>
2023-11-29 11:04:04 +0200

    I added include folder

--- TrainTrip/TrainTrip/Camera.h
+++ TrainTrip/TrainTrip/Camera.h
@@ -2,11 +2,17 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+
 #define GLM_ENABLE_EXPERIMENTAL
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h> 
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+//#include <GLM.hpp>
 
 enum Camera_Movement {
     FORWARD,
@@ -19,5 +25,15 @@ enum Camera_Movement {
 
 class Camera
 {
+
+public:
+   /*
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+    */
+
 };
 #endif
