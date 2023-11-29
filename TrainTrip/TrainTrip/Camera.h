3a18bed20b578001a94a1b3fd5ad47a0eb36de3b
Tibicuy <tiby.onofrei@student.unitbv.ro>
2023-11-29 11:07:43 +0200

    I added some values for Camera

--- TrainTrip/TrainTrip/Camera.h
+++ TrainTrip/TrainTrip/Camera.h
@@ -13,6 +13,8 @@
 #include <fstream>
 #include <sstream>
 //#include <GLM.hpp>
+#include <vector>
+
 
 enum Camera_Movement {
     FORWARD,
@@ -23,17 +25,34 @@ enum Camera_Movement {
     DOWN
 };
 
+
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 200.0f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM  = 60.0f;
+
+
 class Camera
 {
 
 public:
-   /*
+   
     glm::vec3 Position;
     glm::vec3 Front;
     glm::vec3 Up;
     glm::vec3 Right;
     glm::vec3 WorldUp;
-    */
+    
+    
+    float Yaw;
+    float Pitch;
 
+    
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
 };
+
+
 #endif
