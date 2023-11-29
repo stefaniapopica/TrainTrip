f9994962e3e427e46328ba4fdebaf6e0e4410816
Tibicuy <tiby.onofrei@student.unitbv.ro>
2023-11-29 11:12:42 +0200

    Update Camera vector function in  Camera.cpp

--- TrainTrip/TrainTrip/Camera.cpp
+++ TrainTrip/TrainTrip/Camera.cpp
@@ -1,1 +1,14 @@
 #include "Camera.h"
+
+void Camera::updateCameraVectors()
+{
+	// calculate the new Front vector
+	glm::vec3 front;
+	front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+	front.y = sin(glm::radians(Pitch));
+	front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+	Front = glm::normalize(front);
+	// also re-calculate the Right and Up vector
+	Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+	Up = glm::normalize(glm::cross(Right, Front));
+}
