fea8f7233aeab2aeaa54e40307dd771607f9082c
stefaniapopica <stefania.popica@gmail.com>
2023-12-02 14:41:02 +0200

    Model class created libraries

---
+++ TrainTrip/TrainTrip/Model.h
@@ -0,0 +1,23 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <stb_image.h>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "Mesh.h"
+#include "Shader.h"
+
+#include <string>
+#include <vector>
+
+class Model
+{
+public:
+private:
+
+};
+
+#endif
