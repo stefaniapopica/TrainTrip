ae70a8e4f959aadc7edc10a87bd4084f02e6b1c8
stefaniapopica <stefania.popica@gmail.com>
2023-12-02 14:43:51 +0200

    Private functions definition

---
+++ TrainTrip/TrainTrip/Model.cpp
@@ -0,0 +1,30 @@
+#include "Model.h"
+
+void Model::loadModel(string const& path)
+{
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+    
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+    {
+        cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << endl;
+        return;
+    }
+    
+    directory = path.substr(0, path.find_last_of('/'));
+
+    processNode(scene->mRootNode, scene);
+}
+
+void Model::processNode(aiNode* node, const aiScene* scene)
+{
+    for (unsigned int i = 0; i < node->mNumMeshes; i++)
+    {
+        aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+        meshes.push_back(processMesh(mesh, scene));
+    }
+    for (unsigned int i = 0; i < node->mNumChildren; i++)
+    {
+        processNode(node->mChildren[i], scene);
+    }
+}
--- TrainTrip/TrainTrip/Model.h
+++ TrainTrip/TrainTrip/Model.h
@@ -17,6 +17,10 @@ class Model
 {
 public:
 private:
+    void loadModel(string const& path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName);
 
 };
 
