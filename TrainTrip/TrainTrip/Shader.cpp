4361072eee82ffc413b39db2d91244cb41117b30
lucianamihai <luciana.mihai@student.unitbv.ro>
2023-12-09 12:00:19 +0200

    Shader Class CheckCompileErrors modified

--- TrainTrip/TrainTrip/Shader.cpp
+++ TrainTrip/TrainTrip/Shader.cpp
@@ -73,5 +73,13 @@ void Shader::checkCompileErrors(GLuint shader, std::string type)
 			std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
 		}
 	}
-	
+	else
+	{
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
 }
