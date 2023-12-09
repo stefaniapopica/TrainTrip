ceda4443952dcdfa82ab7b7f3f5b3aa6fd79ffbb
Tibicuy <tiby.onofrei@student.unitbv.ro>
2023-12-09 11:30:38 +0200

    declared some function in camera.h

--- TrainTrip/TrainTrip/Camera.h
+++ TrainTrip/TrainTrip/Camera.h
@@ -24,6 +24,13 @@
 #include <ext.hpp>
 
 
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+
+#include <vector>
+
 enum Camera_Movement {
     FORWARD,
     BACKWARD,
@@ -61,6 +68,12 @@ public:
     float MouseSensitivity;
     float Zoom;
 
+    glm::mat4 GetViewMatrix();
+
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
+    void ProcessMouseScroll(float yoffset);
+    void setViewMatrix(glm::vec3 pos);
 
 
     Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) :
