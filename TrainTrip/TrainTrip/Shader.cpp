bb09548594299be6fe449f09e2e1891667f82a78
lucianamihai <luciana.mihai@student.unitbv.ro>
2023-12-09 12:03:23 +0200

    Shader Class constructor modified

--- TrainTrip/TrainTrip/Shader.cpp
+++ TrainTrip/TrainTrip/Shader.cpp
@@ -1,11 +1,17 @@
 #include "Shader.h"
 
-
-
-
-
 Shader::Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath)
 {
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::string geometryCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+	std::ifstream gShaderFile;
+	// exceptions
+	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 
 
 }
