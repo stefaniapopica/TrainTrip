498da8fab590ffe1d3877445459434327e272ae5
alexandra-marin29 <alexandra-a.marin@student.unitbv.ro>
2023-12-09 11:00:09 +0200

    Mesh class draw method modified

--- TrainTrip/TrainTrip/Mesh.cpp
+++ TrainTrip/TrainTrip/Mesh.cpp
@@ -24,6 +24,21 @@ void Mesh::Draw(Shader& shader)
         // retrieve texture number (the N in diffuse_textureN)
         string number;
         string name = textures[i].type;
+
+        if (name == "texture_diffuse")
+          number = std::to_string(diffuseNr++);
+        else if (name == "texture_specular")
+          number = std::to_string(specularNr++); // transfer unsigned int to string
+        else if (name == "texture_normal")
+           number = std::to_string(normalNr++); // transfer unsigned int to string
+        else if (name == "texture_height")
+           number = std::to_string(heightNr++); // transfer unsigned int to string
+
+    // now set the sampler to the correct texture unit
+      glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
+    // and finally bind the texture
+      glBindTexture(GL_TEXTURE_2D, textures[i].id);
+ 
     }
 }
 
