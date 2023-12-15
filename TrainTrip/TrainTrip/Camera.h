c41487e8fa85981c2eea19f633d7ecdb9f2d63f9
Tibicuy <tiby.onofrei@student.unitbv.ro>
2023-12-15 10:54:28 +0200

    added camera constructor in camera.h

--- TrainTrip/TrainTrip/Camera.h
+++ TrainTrip/TrainTrip/Camera.h
@@ -85,6 +85,15 @@ public:
         Pitch = pitch;
         updateCameraVectors();
     }
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) :
+        Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = glm::vec3(posX, posY, posZ);
+        WorldUp = glm::vec3(upX, upY, upZ);
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
     void printPosition();
 private:
 
