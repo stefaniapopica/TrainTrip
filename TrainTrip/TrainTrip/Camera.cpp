cd261e5007f20979853ecbc38859993fc6c5211a
Tibicuy <tiby.onofrei@student.unitbv.ro>
2023-12-15 10:56:22 +0200

    added processKeyboard function in camera.cpp

--- TrainTrip/TrainTrip/Camera.cpp
+++ TrainTrip/TrainTrip/Camera.cpp
@@ -4,6 +4,26 @@ glm::mat4 Camera::GetViewMatrix()
 {
 	return glm::lookAt(Position, Position + Front, Up);
 }
+
+
+void Camera::ProcessKeyboard(Camera_Movement direction, float deltaTime)
+{
+	float velocity = MovementSpeed * deltaTime;
+	if (direction == FORWARD)
+		Position += Front * velocity;
+	if (direction == BACKWARD)
+		Position -= Front * velocity;
+	if (direction == LEFT)
+		Position -= Right * velocity;
+	if (direction == RIGHT)
+		Position += Right * velocity;
+	if (direction == UP)
+		Position += Up * velocity;
+	if (direction == DOWN)
+		Position -= Up * velocity;
+}
+
+
 void Camera::setViewMatrix(glm::vec3 pos)
 {
 	Position = pos;
