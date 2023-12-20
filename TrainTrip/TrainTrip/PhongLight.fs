acf5c0a543011ccffcbbe4e40fc9aba3cc46258b
stefaniapopica <stefania.popica@gmail.com>
2023-12-20 20:55:57 +0200

    Set uniform variable

--- TrainTrip/TrainTrip/PhongLight.fs
+++ TrainTrip/TrainTrip/PhongLight.fs
@@ -9,8 +9,16 @@ uniform vec3 viewPos;
 uniform vec3 lightColor;
 uniform vec3 objectColor;
 
+uniform float ka=0.1;
+uniform float kd=0.9;
+uniform float n=2;
+uniform float ks=0.5;
+
 void  main()
 {
 	vec3 ambient=(lightColor*ka);
+
+    vec3 lightDir=normalize(lightPos-FragPos);
+    vec3 diffuse=lightColor*kd*max(dot(Normal,lightDir),0.0);
 
 }
