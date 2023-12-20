753f5731826be670794e39f847ed9a1966d78887
stefaniapopica <stefania.popica@gmail.com>
2023-12-20 20:54:44 +0200

    Uniform variable declaration

--- TrainTrip/TrainTrip/PhongLight.fs
+++ TrainTrip/TrainTrip/PhongLight.fs
@@ -4,7 +4,13 @@ out vec4 FragColor;
 in vec3 Normal;
 in vec3 FragPos;
 
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
 void  main()
 {
+	vec3 ambient=(lightColor*ka);
 
 }
