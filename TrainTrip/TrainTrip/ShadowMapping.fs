2db43a444c6e92e328ef3c5dd6f6cc0da8861aa9
alexandra-marin29 <alexandra-a.marin@student.unitbv.ro>
2023-12-26 20:01:28 +0200

    ShadowMapping variable declaration

---
+++ TrainTrip/TrainTrip/ShadowMapping.fs
@@ -0,0 +1,15 @@
+#version 330 core
+out vec4 FragColor;
+
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+    vec4 FragPosLightSpace;
+} fs_in;
+
+uniform sampler2D diffuseTexture;
+uniform sampler2D shadowMap;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
