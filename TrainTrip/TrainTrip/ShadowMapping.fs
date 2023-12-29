0bb8ab28dc672410a42ddfd9a959ec4dc82a6a43
lucianamihai <luciana.mihai@student.unitbv.ro>
2023-12-29 20:27:31 +0200

    main in ShadowMapping.fs modified

--- TrainTrip/TrainTrip/ShadowMapping.fs
+++ TrainTrip/TrainTrip/ShadowMapping.fs
@@ -45,5 +45,13 @@ float ShadowCalculation(vec4 FragPosLightSpace)
 
 void main()
 {
-
+     vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
+    vec3 normal = normalize(fs_in.Normal);
+    vec3 lightColor = vec3(0.3);
+    // ambient
+    vec3 ambient = 0.3 * color;
+    // diffuse
+    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * lightColor;
 }
