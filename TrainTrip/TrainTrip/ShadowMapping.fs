642a9567aaf158c09b8e93c5d315f43e543c9dbe
lucianamihai <luciana.mihai@student.unitbv.ro>
2023-12-29 20:28:08 +0200

    ShadowMapping.fs finished

--- TrainTrip/TrainTrip/ShadowMapping.fs
+++ TrainTrip/TrainTrip/ShadowMapping.fs
@@ -54,4 +54,12 @@ void main()
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     float diff = max(dot(lightDir, normal), 0.0);
     vec3 diffuse = diff * lightColor;
+     // specular
+    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = 0.0;
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
+    vec3 specular = spec * lightColor;    
+    
 }
